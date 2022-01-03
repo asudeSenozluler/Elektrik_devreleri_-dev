e1755204eea24f130b7594cb00ef074ddaa010f1
Asude Şenözlüler <senozlulerasude@gmail.com>
2022-01-03 14:34:18 +0300

    first commit

---
+++ Elektrik_devreleri_ödev/Form1.Designer.cs
@@ -0,0 +1,444 @@
+﻿namespace Elektrik_devreleri_ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtV = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtL = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.btnxlxc = new System.Windows.Forms.Button();
+            this.btnakım = new System.Windows.Forms.Button();
+            this.txtHz = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtır = new System.Windows.Forms.TextBox();
+            this.txtxc = new System.Windows.Forms.TextBox();
+            this.txtxl = new System.Windows.Forms.TextBox();
+            this.txtıc = new System.Windows.Forms.TextBox();
+            this.txtıl = new System.Windows.Forms.TextBox();
+            this.btntemizle = new System.Windows.Forms.Button();
+            this.txtq = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btntem2 = new System.Windows.Forms.Button();
+            this.txtI = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(633, 315);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "V:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "R:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "L:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "C:";
+            // 
+            // txtV
+            // 
+            this.txtV.Location = new System.Drawing.Point(55, 353);
+            this.txtV.Name = "txtV";
+            this.txtV.Size = new System.Drawing.Size(149, 30);
+            this.txtV.TabIndex = 5;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(311, 400);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(158, 30);
+            this.txtC.TabIndex = 6;
+            // 
+            // txtL
+            // 
+            this.txtL.Location = new System.Drawing.Point(311, 350);
+            this.txtL.Name = "txtL";
+            this.txtL.Size = new System.Drawing.Size(158, 30);
+            this.txtL.TabIndex = 7;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(55, 400);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(149, 30);
+            this.txtR.TabIndex = 8;
+            // 
+            // btnxlxc
+            // 
+            this.btnxlxc.BackColor = System.Drawing.Color.White;
+            this.btnxlxc.Location = new System.Drawing.Point(517, 425);
+            this.btnxlxc.Name = "btnxlxc";
+            this.btnxlxc.Size = new System.Drawing.Size(128, 62);
+            this.btnxlxc.TabIndex = 10;
+            this.btnxlxc.Text = "XL ve XC hesapla";
+            this.btnxlxc.UseVisualStyleBackColor = false;
+            this.btnxlxc.Click += new System.EventHandler(this.btnxlxc_Click);
+            // 
+            // btnakım
+            // 
+            this.btnakım.BackColor = System.Drawing.Color.White;
+            this.btnakım.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnakım.Location = new System.Drawing.Point(517, 343);
+            this.btnakım.Name = "btnakım";
+            this.btnakım.Size = new System.Drawing.Size(128, 62);
+            this.btnakım.TabIndex = 11;
+            this.btnakım.Text = "Akım Hesapla";
+            this.btnakım.UseVisualStyleBackColor = false;
+            this.btnakım.Click += new System.EventHandler(this.btnakım_Click);
+            // 
+            // txtHz
+            // 
+            this.txtHz.Location = new System.Drawing.Point(55, 449);
+            this.txtHz.Name = "txtHz";
+            this.txtHz.Size = new System.Drawing.Size(149, 30);
+            this.txtHz.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 452);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Hz:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(656, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "I R:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(656, 89);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "I L:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(654, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "I C:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(651, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 25);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "X L:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(651, 199);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 25);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "X C:";
+            // 
+            // txtır
+            // 
+            this.txtır.Location = new System.Drawing.Point(702, 48);
+            this.txtır.Name = "txtır";
+            this.txtır.Size = new System.Drawing.Size(152, 30);
+            this.txtır.TabIndex = 19;
+            // 
+            // txtxc
+            // 
+            this.txtxc.Location = new System.Drawing.Point(702, 194);
+            this.txtxc.MaxLength = 6;
+            this.txtxc.Name = "txtxc";
+            this.txtxc.Size = new System.Drawing.Size(152, 30);
+            this.txtxc.TabIndex = 20;
+            // 
+            // txtxl
+            // 
+            this.txtxl.Location = new System.Drawing.Point(702, 158);
+            this.txtxl.MaxLength = 6;
+            this.txtxl.Name = "txtxl";
+            this.txtxl.Size = new System.Drawing.Size(152, 30);
+            this.txtxl.TabIndex = 21;
+            // 
+            // txtıc
+            // 
+            this.txtıc.Location = new System.Drawing.Point(702, 120);
+            this.txtıc.Name = "txtıc";
+            this.txtıc.Size = new System.Drawing.Size(152, 30);
+            this.txtıc.TabIndex = 22;
+            // 
+            // txtıl
+            // 
+            this.txtıl.Location = new System.Drawing.Point(702, 84);
+            this.txtıl.Name = "txtıl";
+            this.txtıl.Size = new System.Drawing.Size(152, 30);
+            this.txtıl.TabIndex = 23;
+            // 
+            // btntemizle
+            // 
+            this.btntemizle.BackColor = System.Drawing.Color.White;
+            this.btntemizle.Location = new System.Drawing.Point(311, 441);
+            this.btntemizle.Name = "btntemizle";
+            this.btntemizle.Size = new System.Drawing.Size(158, 46);
+            this.btntemizle.TabIndex = 24;
+            this.btntemizle.Text = "Temizle";
+            this.btntemizle.UseVisualStyleBackColor = false;
+            this.btntemizle.Click += new System.EventHandler(this.btntemizle_Click);
+            // 
+            // txtq
+            // 
+            this.txtq.Location = new System.Drawing.Point(702, 230);
+            this.txtq.Name = "txtq";
+            this.txtq.Size = new System.Drawing.Size(152, 30);
+            this.txtq.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(671, 235);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(36, 25);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Q:";
+            // 
+            // btntem2
+            // 
+            this.btntem2.BackColor = System.Drawing.Color.White;
+            this.btntem2.Location = new System.Drawing.Point(702, 302);
+            this.btntem2.Name = "btntem2";
+            this.btntem2.Size = new System.Drawing.Size(152, 46);
+            this.btntem2.TabIndex = 27;
+            this.btntem2.Text = "Temizle";
+            this.btntem2.UseVisualStyleBackColor = false;
+            this.btntem2.Click += new System.EventHandler(this.btntem2_Click);
+            // 
+            // txtI
+            // 
+            this.txtI.Location = new System.Drawing.Point(702, 266);
+            this.txtI.Name = "txtI";
+            this.txtI.Size = new System.Drawing.Size(152, 30);
+            this.txtI.TabIndex = 29;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(678, 271);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(25, 25);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "I:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(690, 437);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(131, 25);
+            this.label13.TabIndex = 30;
+            this.label13.Text = "Q=Faz Açısı";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(699, 462);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(155, 25);
+            this.label14.TabIndex = 31;
+            this.label14.Text = "I=Bileşke Akım";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(860, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 110);
+            this.textBox1.TabIndex = 32;
+            this.textBox1.Text = "Lütfen doğru sonuç için birimlere dikkat ediniz!";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(860, 185);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 163);
+            this.textBox2.TabIndex = 33;
+            this.textBox2.Text = "Lütfen doğru sonuç için öncelikle XL ve XC hesapla butonuna tıklayınız!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1042, 507);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtI);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btntem2);
+            this.Controls.Add(this.txtq);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btntemizle);
+            this.Controls.Add(this.txtıl);
+            this.Controls.Add(this.txtıc);
+            this.Controls.Add(this.txtxl);
+            this.Controls.Add(this.txtxc);
+            this.Controls.Add(this.txtır);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtHz);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnakım);
+            this.Controls.Add(this.btnxlxc);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtL);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtV);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paralel R ve L Devreleri ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtV;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtL;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Button btnxlxc;
+        private System.Windows.Forms.Button btnakım;
+        private System.Windows.Forms.TextBox txtHz;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtır;
+        private System.Windows.Forms.TextBox txtxc;
+        private System.Windows.Forms.TextBox txtxl;
+        private System.Windows.Forms.TextBox txtıc;
+        private System.Windows.Forms.TextBox txtıl;
+        private System.Windows.Forms.Button btntemizle;
+        private System.Windows.Forms.TextBox txtq;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btntem2;
+        private System.Windows.Forms.TextBox txtI;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Elektrik_devreleri_ödev/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elektrik_devreleri_ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btntemizle_Click(object sender, EventArgs e)
+        {
+            txtV.Text = "";
+            txtR.Text = "";
+            txtL.Text = "";
+            txtR.Text = "";
+            txtHz.Text = "";
+            txtC.Text = "";
+        }
+
+        private void btntem2_Click(object sender, EventArgs e)
+        {
+            txtır.Text = "";
+            txtıc.Text = "";
+            txtıl.Text = "";
+            txtxc.Text = "";
+            txtxl.Text = "";
+            txtq.Text = "";
+            txtI.Text = "";
+        }
+
+        private void btnxlxc_Click(object sender, EventArgs e)
+        {
+            if (txtR.Text == "" || txtV.Text == "" || txtC.Text == "" || txtL.Text == "" || txtHz.Text == "")
+            {
+                MessageBox.Show("Lütfen istenilen yerleri eksiksiz doldurunuz!");
+            }
+            double xl, xc,frekans, l,c;
+            const double pi=3.14159;
+            l = Convert.ToDouble(txtL.Text);
+            frekans = Convert.ToDouble(txtHz.Text);
+            xl = 2 * l * frekans * pi;
+            txtxl.Text = xl.ToString();
+            c = Convert.ToDouble(txtC.Text);
+            xc = 1 / (2 * pi * frekans * c);
+            txtxc.Text = xc.ToString();
+
+        }
+
+        private void btnakım_Click(object sender, EventArgs e)
+        {
+            if(txtR.Text==""|| txtV.Text==""||txtC.Text==""||txtL.Text==""|| txtHz.Text=="")
+            {
+                MessageBox.Show("Lütfen istenilen yerleri eksiksiz doldurunuz!");
+            }
+            double xl, xc, v, Ir, Ic, Il,R,bileske,islem,fazacısı,Q;
+            xl = Convert.ToDouble(txtxl.Text);
+            xc = Convert.ToDouble(txtxc.Text);
+            v = Convert.ToDouble(txtV.Text);
+            R=Convert.ToDouble(txtR.Text);
+            Ir = v / R;
+            txtır.Text = Ir.ToString();
+            Il = v / xl;
+            txtıl.Text = Il.ToString();
+            Ic = v / xc;
+            txtıc.Text = Ic.ToString();
+            islem =( Ir * Ir) +( (Il - Ic) * (Il - Ic));
+            bileske = Math.Sqrt(islem);
+            txtI.Text = bileske.ToString();
+            fazacısı = (Il - Ic) / Ir;
+            Q = Math.Atan(fazacısı);
+            txtq.Text = Q.ToString();
+
+
+
+
+        }
+    }
+}
